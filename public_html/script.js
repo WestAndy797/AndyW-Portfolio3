b6eef8b49f59a2d86875ac9d210b83f89e767bec
Andy West <captainrex2000@gmail.com>
2015-01-07 14:46:51 -0800

    2nd commit 1/7/14

--- public_html/script.js
+++ public_html/script.js
@@ -3,8 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+/* Asthetics for different calsses or ids ex: sibling or nvidia */
 $("document").ready(function(){
    $(".sibling").css("background-color", "gold"); 
-    
+   $('p:last').css("background-color", "purple");
+   $('#nvidia').bind('click', alertButtonClick);
+   
 });
+/* Functions for different IDs */
+function alertButtonClick(){
+    alert("You Pressed The Secret Button")
+    
+}
